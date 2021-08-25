46b9b31acbcda200b61984ca98463453664f5402
ryimo <karyo402_0312@yahoo.co.jp>
2021-08-25 23:52:50 +0900

    第二弾リリース ・検索結果ページ内で価格の判定を行うようになった ・価格の判定で下限も判定するようになった（購入するのは、下限価格＜＝実際の価格＜＝上限価格　のとき） ・販売元を取得・判定しなくなった（エクセルからも削除）

--- 完成品/ツール/AutoPurchase.py
+++ 完成品/ツール/AutoPurchase.py
@@ -15,7 +15,6 @@ import time
 # グローバル変数
 UserName_amazon = ""
 Password_amazon = ""
-Favorite_sellers = []
 IsHideChrome = False
 
 # 実行フォルダパスを取得
@@ -55,11 +54,12 @@ def write_Excel(productName, price):
 class AmazonThreading(threading.Thread):
 
     # コンストラクタ
-    def __init__(self, thread_name, productName, url, price):
+    def __init__(self, thread_name, productName, url, minPrice, maxPrice):
         self.thread_name = str(thread_name)
         self.productName = productName
         self.url = url
-        self.price = int(price)
+        self.minPrice = int(minPrice)
+        self.maxPrice = int(maxPrice)
 
         threading.Thread.__init__(self)
 
@@ -82,7 +82,7 @@ class AmazonThreading(threading.Thread):
             if IsHideChrome:
                 options.add_argument('--headless')
             driver = webdriver.Chrome(executable_path=chromDirverPath, chrome_options=options)
-            wait = WebDriverWait(driver, 20)
+            wait = WebDriverWait(driver, 10)
             
             # [S] Amazonへのログイン
             driver.get("https://www.amazon.co.jp")
@@ -121,109 +121,111 @@ class AmazonThreading(threading.Thread):
             quit_flg = False
             while quit_flg == False:
 
-                # ページ内のh2タグを全部取得
+                # ページ内の商品情報divタグを取得
                 wait.until(EC.visibility_of_all_elements_located((By.TAG_NAME, 'h2')))
-                h2TagElements = driver.find_elements_by_tag_name("h2")
-                for h2TagEle in h2TagElements:
+                # 商品情報divタグを判定
+                divTag_css_selector=''
+                if len(driver.find_elements_by_css_selector('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20')) != 0:
+                    divTag_css_selector = 'div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20'
+                elif len(driver.find_elements_by_css_selector('div.s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.sg-col-12-of-16')) != 0:
+                    divTag_css_selector = 'div.s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.sg-col-12-of-16'
+                
+                divTagElements = driver.find_elements_by_css_selector(divTag_css_selector)
+                
+                for divTagEle in divTagElements:
                     
-                    # 取得したh2タグに含まれる商品名を取得
+                    # 取得した商品情報divタグに含まれる商品名を取得
                     h2Tag_css_selector=''
-                    if len(h2TagEle.find_elements_by_css_selector('span.a-size-base-plus.a-color-base.a-text-normal')) != 0:
+                    if len(divTagEle.find_elements_by_css_selector('span.a-size-base-plus.a-color-base.a-text-normal')) != 0:
                         h2Tag_css_selector = 'span.a-size-base-plus.a-color-base.a-text-normal'
-                    elif len(h2TagEle.find_elements_by_css_selector('span.a-size-medium.a-color-base.a-text-normal')) != 0:
+                    elif len(divTagEle.find_elements_by_css_selector('span.a-size-medium.a-color-base.a-text-normal')) != 0:
                         h2Tag_css_selector = 'span.a-size-medium.a-color-base.a-text-normal'
 
                     if h2Tag_css_selector != '':
-                        productNameElement = h2TagEle.find_element_by_css_selector(h2Tag_css_selector)
+                        productNameElement = divTagEle.find_element_by_css_selector(h2Tag_css_selector)
                         productNameElementText = str(productNameElement.text)
 
                         # 指定された商品名が含まれているか確認
                         if self.productName in productNameElementText:
                             
-                            # 商品ページに移動
-                            if len(h2TagEle.find_elements_by_tag_name("a")) != 0:
-                                h2TagEle.find_element_by_tag_name("a").click()
-                                driver.switch_to.window(driver.window_handles[1])
-                                wait.until(EC.visibility_of_element_located((By.ID, 'tabular-buybox-container')))
-                                
-                                #購入フラグ初期化
-                                buyFlg = False
-
-                                # 販売元・価格を取得
-                                buyboxElement = driver.find_element_by_id('tabular-buybox-container')
-                                priceText = driver.find_element_by_id('priceblock_ourprice').text
+                            if len(divTagEle.find_elements_by_class_name('a-price-whole')) != 0:
+                                priceText = divTagEle.find_element_by_class_name('a-price-whole').text
                                 price_webPage = int(priceText.replace('￥', '').replace(',', ''))
+                            
+                                # 価格判定
+                                if self.minPrice <= price_webPage and price_webPage <= self.maxPrice:
+                                    
+                                    # 商品ページに移動
+                                    if len(divTagEle.find_elements_by_tag_name("a")) != 0:
+                                        divTagEle.find_element_by_tag_name("a").click()
+                                        driver.switch_to.window(driver.window_handles[1])
+                                        wait.until(EC.visibility_of_element_located((By.ID, 'buy-now-button')))
+                                                        
+                                        # 購入処理
+                                        if len(driver.find_elements_by_id("buy-now-button")) != 0:
+                                            driver.find_element_by_id("buy-now-button").click()
 
-                                # 販売元を判定（エクセルから取得した即買い販売者名と比較）
-                                if len(buyboxElement.find_elements_by_tag_name("a")) != 0:
-                                    sellerName = buyboxElement.find_element_by_tag_name("a").get_attribute("textContent")
-                                    for sellerName_excel in Favorite_sellers:
-                                        if sellerName == sellerName_excel:
-                                            buyFlg = True
-                                            break
+                                            try:
+                                                # 商品ページに小窓が出てくるパターン（おそらく購入だとこっち）
+                                                
+                                                wait.until(EC.visibility_of_element_located((By.ID, 'turbo-checkout-iframe')))
+                                                if len(driver.find_elements_by_id('turbo-checkout-iframe')) != 0:
+                                                    # フレーム移動
+                                                    driver.switch_to.frame(driver.find_element_by_id('turbo-checkout-iframe'))
 
-                                # 価格を判定（すでにbuyFlgが立っているならスキップ）
-                                if buyFlg == False:
-                                    if price_webPage <= self.price:
-                                        buyFlg = True
-                                
-                                # 購入処理
-                                if buyFlg:
-                                    if len(driver.find_elements_by_id("buy-now-button")) != 0:
-                                        driver.find_element_by_id("buy-now-button").click()
+                                                    if len(driver.find_elements_by_id('turbo-checkout-pyo-button')) != 0:
+                                                        # 購入
+                                                        driver.find_element_by_id('turbo-checkout-pyo-button').click()
 
-                                        try:
-                                            # 商品ページに小窓が出てくるパターン（おそらく購入だとこっち）
-                                            
-                                            wait.until(EC.visibility_of_element_located((By.ID, 'turbo-checkout-iframe')))
-                                            if len(driver.find_elements_by_id('turbo-checkout-iframe')) != 0:
-                                                # フレーム移動
-                                                driver.switch_to.frame(driver.find_element_by_id('turbo-checkout-iframe'))
+                                                        # 購入履歴を記載
+                                                        write_Excel(productNameElementText, price_webPage)
+                                                    
+                                                    #　親フレームに戻す
+                                                    driver.switch_to.default_content()
+                                            except:
+                                                # 商品ページから移動して購入を確定するパターン（おそらく予約だとこっち）
 
-                                                if len(driver.find_elements_by_id('turbo-checkout-pyo-button')) != 0:
+                                                # プライム会員の誘いをスキップ  
+                                                if len(driver.find_elements_by_xpath('//*[@id="primeAutomaticPopoverAdContent"]/div/div[1]/div[1]/a')) != 0:
+                                                    driver.find_element_by_xpath('//*[@id="primeAutomaticPopoverAdContent"]/div/div[1]/div[1]/a').click()
+
+                                                if len(driver.find_elements_by_css_selector('input.a-button-text.place-your-order-button')) != 0:
                                                     # 購入
-                                                    driver.find_element_by_id('turbo-checkout-pyo-button').click()
+                                                    driver.find_element_by_css_selector('input.a-button-text.place-your-order-button').click()
 
                                                     # 購入履歴を記載
-                                                    write_Excel(productNameElementText, price_webPage)
-                                                
-                                                #　親フレームに戻す
-                                                driver.switch_to.default_content()
-                                        except:
-                                            # 商品ページから移動して購入を確定するパターン（おそらく予約だとこっち）
+                                                    write_Excel(productNameElementText, price_webPage)                                        
 
-                                            # プライム会員の誘いをスキップ  
-                                            if len(driver.find_elements_by_xpath("//*[@id=""primeAutomaticPopoverAdContent""]/div/div[1]/div[1]/a")) != 0:
-                                                driver.find_element_by_xpath("//*[@id=""primeAutomaticPopoverAdContent""]/div/div[1]/div[1]/a").click()
+                                            quit_flg = True
+                                    
+                                    # 商品ページを閉じる
+                                    driver.close()
+                                    driver.switch_to.window(driver.window_handles[0]) 
 
-                                            if len(driver.find_elements_by_css_selector('input.a-button-text.place-your-order-button')) != 0:
-                                                # 購入
-                                                driver.find_element_by_css_selector('input.a-button-text.place-your-order-button').click()
-
-                                                # 購入履歴を記載
-                                                write_Excel(productNameElementText, price_webPage)                                        
-
-                                        quit_flg = True
-                                
-                                # 商品ページを閉じる
-                                driver.close()
-                                driver.switch_to.window(driver.window_handles[0]) 
-
-                                if quit_flg:
-                                    break
+                                    if quit_flg:
+                                        break
 
                 # ループ終了判定
                 if quit_flg == False:
 
-                    # エクセルが終了通知用のファイルを作成することで、終了指示を検知する
-                    if(os.path.isfile(os.path.join(ExecDir,'loop_end.txt'))):
-                        quit_flg = True
-                    else:
-                        # あまり連続して更新をかけると怒られそうなので、1分待機
-                        time.sleep(60)
+                    LOOP_NUM = 12
+                    i = 1
+                    # あまり連続して更新をかけると怒られそうなので、1分(5秒×12回)待機
+                    while i <= LOOP_NUM:
 
+                        # 10秒待機ごとに終了フラグを確認
+                        # エクセルが終了通知用のファイルを作成することで、終了指示を検知する
+                        if(os.path.isfile(os.path.join(ExecDir,'loop_end.txt'))):
+                            quit_flg = True
+                            break
+                        else:
+                            time.sleep(5)
+                            i += 1
+
+                    if i > LOOP_NUM:
                         # 指定されたURLを再ロードして一覧更新
-                        driver.refresh()
+                            driver.refresh()
+                            
 
         except Exception as e:
             # 例外発生時に内容をファイルに出力する
@@ -261,34 +263,23 @@ def callFromExcel():
     global IsHideChrome
     if int(ws.range((9, 4)).value) == 2:
         IsHideChrome = True
-    
-    # 即買い販売者名を取得
-    RowNum = 9
-    while True:
-        sellerName_excel = ws.range((RowNum, 5)).value
-        
-        if sellerName_excel is None:
-            break
-        else:
-            Favorite_sellers.append(sellerName_excel)
-        
-        RowNum += 1
     # [E] ツール操作シートの情報取得
 
     # [S] 購入希望商品一覧シートの情報取得
     # 商品情報を取得
     ws = wb.sheets('購入希望商品一覧')
-    RowNum = 5
+    RowNum = 6
     excelInfoList = []
     while True:
         productName_excel = ws.range((RowNum, 4)).value
         url_excel = ws.range((RowNum, 5)).value
-        price_excel = ws.range((RowNum, 6)).value
+        minPrice_excel = ws.range((RowNum, 6)).value
+        maxPrice_excel = ws.range((RowNum, 7)).value
         
-        if productName_excel is None or url_excel is None or price_excel is None:
+        if productName_excel is None or url_excel is None or minPrice_excel is None or maxPrice_excel is None:
             break
         else:
-            excelInfoList.append([productName_excel, url_excel, price_excel])
+            excelInfoList.append([productName_excel, url_excel, minPrice_excel, maxPrice_excel])
         
         RowNum += 1
     # [E] 購入希望商品一覧シートの情報取得
@@ -302,7 +293,7 @@ def callFromExcel():
     # 購入希望商品分スレッドを作成し、処理実行
     thList=[]
     for i in range(len(excelInfoList)):
-        thread = AmazonThreading(thread_name=i, productName=excelInfoList[i][0], url=excelInfoList[i][1], price=excelInfoList[i][2])
+        thread = AmazonThreading(thread_name=i, productName=excelInfoList[i][0], url=excelInfoList[i][1], minPrice=excelInfoList[i][2], maxPrice=excelInfoList[i][3])
         thread.start()
         thList.append(thread)
     
